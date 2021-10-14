c2ece2ff953dc612f4ab6f5a1d6a5dbe3a683642
Ednardo-Matos <ednardoinacio@outlook.com>
2021-10-13 23:18:10 -0300

    Instalaçao e configuraçao do  sequelize

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 # npm init
 # npm install express --save
 # npm install -- save-dev nodemon
-# 
-#
+# npm install --save sequelize
+# npm install --save mysql2
 #
 #
 #
--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const express = require('express') //Carregando na memoria
 const app = express() //Fazendo a inicialização
 const port = 3000 //Criando uma porta do servidor
 
+const { Sequelize } = require('sequelize')
+const sequelize = new Sequelize('database',
+    'root',
+    'alice*23122020',
+    { dialect: 'mysql' })
+
 const userRouter = require('./routers/user')
 app.use(express.json())//Habilitar express para receber json
 
